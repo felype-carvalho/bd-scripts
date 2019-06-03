48a53c58b3855208273d3f35db577d9b8afd0a31
Felype <felype.oliveira@etec.sp.gov.br>
2019-06-03 20:18:59 -0300

    criei o scrip de manuteção

---
+++ Scripts/manutencao.sql
@@ -0,0 +1,4 @@
+use videogames;
+alter table Funcionarios add check (sexo = upper= 'M' or upper='F');
+alter table Funcionarios modify sexo char(1) null;
+alter table Pessoas modify cpf_pessoa varchar(11) unique; 
